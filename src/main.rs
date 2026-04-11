dc791c5adf5611c9f42c64717952be9c28be8d6f
Ian <ianlum314@gmail.com>
2026-04-11 00:09:02 -0400

    boid sim first draft

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
+mod boid;
+
 use macroquad::prelude::*;
 
 #[macroquad::main("MyGame")]
 async fn main() {
+    let mut boids = Vec::new();
+
+    // Initialize boids
+    for _ in 0..100 {
+        let position = vec2(
+            rand::gen_range(0.0, screen_width()),
+            rand::gen_range(0.0, screen_height()),
+        );
+        let velocity = vec2(
+            rand::gen_range(-100.0, 100.0),
+            rand::gen_range(-100.0, 100.0),
+        );
+        boids.push(boid::Boid::new(position, velocity));
+    }
+
     loop {
-        clear_background(RED);
+        let dt = get_frame_time();
+        clear_background(WHITE);
+        let prev_boids = boids.clone();
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        for boid in &mut boids {
+            boid.update(&prev_boids, dt);
+        }
 
-        draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
-
+        for boid in &boids {
+            boid.draw();
+        }
         next_frame().await
     }
 }
