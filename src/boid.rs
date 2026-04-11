7c71a341c945b3a298b093fa9c93668572ab4aab
Ian <ianlum314@gmail.com>
2026-04-11 18:15:26 -0400

    tweak boid size

--- src/boid.rs
+++ src/boid.rs
@@ -18,11 +18,15 @@ impl Boid {
     }
 
     pub fn draw(&self) {
+        // Isosceles triangle
+        const BASE: f32 = 8.0;
+        const HEIGHT: f32 = 18.0;
+
         let forward = self.velocity.normalize_or_zero();
         let right = vec2(-forward.y, forward.x);
-        let tip = self.position + forward * 18.0;
-        let left_base = self.position - forward * 12.0 - right * 6.0;
-        let right_base = self.position - forward * 12.0 + right * 6.0;
+        let tip = self.position + forward * HEIGHT / 2.0;
+        let left_base = self.position - forward * HEIGHT / 2.0 - right * BASE / 2.0;
+        let right_base = self.position - forward * HEIGHT / 2.0 + right * BASE / 2.0;
 
         draw_triangle(tip, left_base, right_base, DARKGRAY);
     }
