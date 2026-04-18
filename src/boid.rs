65d3e4d630480213ebbb5ee388b8816de89e5a36
Ian <ianlum314@gmail.com>
2026-04-18 17:51:04 -0400

    tweak constants

--- src/boid.rs
+++ src/boid.rs
@@ -3,8 +3,8 @@ use macroquad::prelude::*;
 pub const DESIRED_DISTANCE: f32 = 100.0;
 pub const ATTRACTION_RANGE: f32 = 200.0;
 pub const ATTRACTION_GAIN: f32 = 5.0;
-pub const REPULSION_GAIN: f32 = 5.0;
-pub const BUMP_FLATNESS: f32 = 0.5;
+pub const REPULSION_GAIN: f32 = 8.0;
+pub const BUMP_FLATNESS: f32 = 0.2;
 
 #[derive(Clone)]
 pub struct Boid {
